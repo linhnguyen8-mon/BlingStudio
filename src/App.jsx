e404fee46dcd5d7f9c0fe654c82652ac74aeab69
Nguyen Dieu Linh <linhyuji@gmail.com>
2024-04-04 21:25:46 +0700

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Service from "./layouts/Service";
 import HowItWork from "./layouts/HowItWork";
 import OurWork from "./layouts/OurWork";
 import Footer from "./layouts/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
 import {
     Link,
     Button,
@@ -19,27 +18,25 @@ import {
 function App() {
     return (
         <div className=" w-screen bg-background 4xl:container  mx-auto">
-            <Router>
-                <Navbar />
-                <Element name="header">
-                    <Header />
-                </Element>
-                <Element name="whyus">
-                    <Intro />
-                </Element>
-                <Element name="service">
-                    <Service />
-                </Element>
-                <Element name="howitwork">
-                    <HowItWork />
-                </Element>
-                <Element name="ourwork">
-                    <OurWork />
-                </Element>
-                <Element name="footer">
-                    <Footer />
-                </Element>
-            </Router>
+            <Navbar />
+            <Element name="header">
+                <Header />
+            </Element>
+            <Element name="whyus">
+                <Intro />
+            </Element>
+            <Element name="service">
+                <Service />
+            </Element>
+            <Element name="howitwork">
+                <HowItWork />
+            </Element>
+            <Element name="ourwork">
+                <OurWork />
+            </Element>
+            <Element name="footer">
+                <Footer />
+            </Element>
         </div>
     );
 }
